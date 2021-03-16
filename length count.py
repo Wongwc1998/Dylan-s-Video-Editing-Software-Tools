a8d9b51a88679be5cdf02b62ca4975543debdce6
Composite1618 <37727136+Composite1618@users.noreply.github.com>
2021-03-16 17:52:54 +0800

    Update length count.py

--- length count.py
+++ length count.py
@@ -1,3 +1,4 @@
+#This is the code used to generate the megalovania sound effects. It counts the amount of characters in a line and how much sound effects I should use
 line="It's a beautiful lamp oil outside;Ropes are singing;Bombs are blooming;On days like this, heroes like you;Should come back when you're a little richer"
 line=line.split(";")
 print(line)
