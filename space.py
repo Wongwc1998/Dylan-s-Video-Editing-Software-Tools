9b923b26b5173c75fada6131b479ebe939a48fda
Composite1618 <37727136+Composite1618@users.noreply.github.com>
2021-03-16 17:42:35 +0800

    Update space.py
    Used more list comprehensions and deleted unwanted variables

--- space.py
+++ space.py
@@ -4,23 +4,13 @@ import soundfile as sf
 import pyrubberband as pyrb
 from scipy.io.wavfile import write
 
-notes_raw = []
-length_raw = []
-space_raw = []
-notes = []
-length = []
-space = []
-clips = []
-tempo_ratio=[]
-bpm = 126  # song bpm
-path = r'C:/ytpmv/morshu/'  # midi filepath
-sourcepath = r'C:/ytpmv/morshu/snorm2/'  # filepath
+bpm = 140  # song bpm
+path = r'C:/ytpmv/glados echo/'  # midi filepath
+sourcepath = r'C:/ytpmv/glados echo/norm/'  # filepath
 temppath=r'C:/ytpmv/'
 
-lyrics="Lamp Oil Rope Bombs You want it It's yours my friend As long as2 you2 have e nough ru pees"
-lyrics="Lamp Oil Lamp Oil Lamp Oil Lamp Oil Bombs Ru Pees You want it You want it It's yours my friend As long as2 you2 have e nough ru pees I can't give cre dit Come back when you're Ri cher Lamp2"
-lyrics="Lamp Oil Rope Bombs You want it It's yours As long as2 you2 have e nough ru pees as2 you2 have e nough ru pees Lamp Oil Rope Bombs You want it It's yours As long as2 you2 have e nough ru pees as2 you2 have e nough ru pees Lamp Oil Rope Bombs You want it It's yours As long as2 you2 have e nough ru pees as2 you2 have e nough ru pees Sor ry Link I can't give cre dit Come back when you're Come back when you're a lit tle mmm a lit tle mmm a lit tle mmm Ri cher Ri cher cher"
-
+lyrics="I'm2 gon2 na2 burn my house down in to an ug gly black I'm gon na run aw way now and nev ver look back I'm2 gon2 na2 burn my house down in to an ug gly black I'm gon na run aw way now and nev ver look back I'm2 gon2 na2 burn my house down in to an ug gly black I'm gon na run aw way now and nev ver look back I'm2 gon2 na2 burn my house down in to an ug gly black I'm gon na run aw way now and nev ver look back I'm2 gon2 na2 burn my house down in and nev ver look back and nev ver look back and nev ver look back back"
+lyrics="The clocks stopped tick king for rev ver a go How long have I been up Id don't know Ic can't get ag grip But Ic2 can't2 let go2 There was sn't an ny thing toh hold on toth tho Why can'ti is see Why can'ti is see All thec col lors that you see2 Pleasec can Ib be Pleasec can Ib be Co lor ful and free WHAT THEhe HELL'S GOi ING ON2 CAN2 SOME ONE TELL ME PLEASE WHY2 I'M SWITCH chING FAS TER THAN THE2 CHAN NELS ON3 Tee Vee I'm2 black then I'm3 white No some2 thing2 is2 sn't2 right My e ne my's in vi si ble, Id2 don't2 know2 how2 to fight The3 tre mb ling fear is3 more than2 Ic3 can3 take When I'm4 up2 a2 gainst the4 ec cho in2 the5 mirror ec2 cho2 ahh ahh ahh ahh ahh ahh oh2 ahh ahh ahh ahh oh2 oh2 oh2 oh2 ahh ahh ahh ahh ahh ahh ahh ahh ahh ahh know"
 l_list = lyrics.split()
 concat_list=[]
 concat_string=''
@@ -28,98 +18,31 @@ new_array=[]
 string2=''
 wav_dur_array=[]
 
-thresh_interval=1.0
-thresh_ratio_divide=4.0
-thresh_ratio_merge=thresh_ratio_divide
-long_interval=7.0
+max_ratio=4.0
 done=-1
 
-length_raw=[24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 1560, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 216, 24, 24, 24, 24, 25, 23, 24, 24, 12, 12, 12, 12, 48, 216, 48, 24, 48, 24, 24, 24, 24, 24, 24, 48, 48, 48, 48, 48, 48, 24, 24, 24, 360, 48, 96, 96, 96, 96, 96, 96, 192, 24, 24, 24, 24, 24, 24, 24, 24, 192, 384, 24, 24, 24, 24, 25, 23, 24, 24, 12, 12, 12, 12, 48, 216, 48, 24, 48, 24, 24, 24, 24, 24, 24, 48, 48, 48, 48, 48, 48, 24, 24, 24, 216, 48, 48, 48, 48, 96, 96, 96, 96, 96, 96, 192, 24, 24, 24, 24, 24, 24, 24, 24, 192, 3264, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 72, 48, 72, 48, 47, 49, 24, 24, 72, 48, 72, 48, 47, 49, 24, 24, 5400, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 24, 24, 24, 48, 72, 48, 48, 48, 24, 24, 96, 96]
-length_raw=[24, 24, 24, 24, 24, 24, 24, 24, 48, 48, 216, 48, 24, 48, 24, 24, 24, 24, 24, 24, 48, 48, 48, 48, 48, 48, 24, 24, 24, 216, 48, 48, 48, 48, 96, 96, 96, 96, 96, 96, 96, 96, 96]
-length_raw=[96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 96, 96, 96, 48, 48, 96, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 96, 96]
-print(length_raw)
-space_raw=[0]*1000
+length_raw=[96, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 96, 48, 48, 48, 48, 48, 48, 192, 48, 48, 48, 48, 192, 48, 48, 48, 48, 96, 96]
+length_raw=[96, 96, 96, 48, 96, 48, 48, 48, 48, 192, 48, 48, 48, 48, 48, 192, 48, 96, 144, 96, 96, 48, 48, 96, 48, 48, 48, 96, 192, 48, 48, 48, 48, 48, 96, 48, 48, 48, 96, 240, 96, 48, 96, 144, 96, 48, 96, 144, 96, 48, 96, 144, 96, 96, 192, 96, 48, 96, 144, 96, 48, 96, 144, 96, 48, 96, 144, 288, 48, 48, 96, 48, 48, 96, 96, 48, 48, 48, 48, 192, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 96, 96, 96, 48, 48, 96, 96, 48, 48, 48, 48, 96, 96, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 144, 48, 48, 96, 96, 96, 48, 48, 48, 48, 96, 144, 48, 48, 48, 48, 144, 48, 48, 48, 48, 48, 192, 96, 96, 48, 48, 48, 48, 48, 48, 96, 48, 48, 48, 48, 72, 72, 72, 72, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 96, 96]
 beats = list(np.array(length_raw)/bpm*15/24)
-space = list(np.array(space_raw)/bpm*15/24)
+space = [0.0]*1000
 # math formula for calculating what frame the note should be in.
 # (this line code assumes 24 frames per second)
 
-
 # append 2 because the loop behaves kinda weird if you don't
 space.append(0)
-#print(len(space))
-#print(len(beats))
 
-for i in range (len(beats)):
-    notes.append(space[i]+beats[i])
-
-print(notes)
-for i in range(len(l_list)):
-    wav_file=sourcepath+l_list[i]+".wav"
-    wav_duration=sox.file_info.duration(wav_file)
-    wav_dur_array.append(wav_duration)
-    
-#print(beats)
-#print(wav_dur_array)
-
-
-for i in range(0,min(len(beats),len(wav_dur_array))):
-    #print("%5f, %5f" %(notes[i],wav_dur_array[i]))
-    tempo_ratio.append(wav_dur_array[i]/notes[i])
-print(tempo_ratio)
-    
-'''
-#beats=divide_this(wav_dur_array, beats, thresh_ratio_divide, long_interval)
-#beats=merge_this(wav_dur_array, beats, thresh_ratio_merge)
-    
-#for i in range(1,99):
-    #beats=divide_this(wav_dur_array, beats, thresh_ratio_divide, long_interval)
-    
-#print(beats)
-#print(space)
-
-#print(wav_dur_array)
-for i in range(0,min(len(beats),len(l_list))):
-    if (beats[i]/wav_dur_array[i])>thresh_ratio_merge:
-        space[i]=space[i]+(beats[i]-wav_dur_array[i]*thresh_ratio_merge)
-        new_array.append(wav_dur_array[i]*thresh_ratio_merge)
-    else:
-        new_array.append(beats[i])
-    #print(str(i)+' ' + str(new_array[i])+' '+str(space[i]))
-    #sum_duration=temp_int
-    #print(str(tempo_ratio)+' '+l_list[i]+' '+str(wav_dur_array[i]))
-    #tfm = sox.Transformer()
-    #tfm.tempo(tempo_ratio)
-    #concat_string=sourcepath+'temp/'+str(i)+'.wav'
-    #concat_list.append(concat_string)
-    #tfm.build(input_filepath=wav_file,output_filepath=concat_string)
-
-#print(concat_list)
-
-
-#print(beats)
-#print(l_list)
-
-#print(new_array)
-new_array[-1]=1
-
-
-
-#print(new_array)
-#print(l_list)
-#print(space)
-for i in range(0,len(space)):
-    if space[i]<0.0:
-        print("something wrong: " + str(space[i])+' '+l_list[i]+' '+str(wav_dur_array[i])+' '+str(beats[i]))
-
-#print(tempo_ratio)
+notes = [a+b for a,b in zip(space, beats)]
+wav_dur_array=[sox.file_info.duration(sourcepath+i+".wav") for i in l_list]
+tempo_ratio=[a/b for a,b in zip(wav_dur_array, notes)]
+new_array=[b*max_ratio if a/b > max_ratio else a for a,b in zip(beats, wav_dur_array)]
+space=[a-b*max_ratio if a/b > max_ratio else 0.0 for a,b in zip(beats, wav_dur_array)]
+#changed append to listcomp cause I'm reading fluent python
 
 for i in range(0,min(len(beats),len(l_list))-1):
     if i > done:
         wav_file=sourcepath+l_list[i]+".wav"
         tfm = sox.Transformer()
         concat_string=temppath+'temp/'+l_list[i]+str(i)+'.wav'
-        #print(str(wav_dur_array[i]/new_array[i])+' '+str(new_array[i])+' '+str(wav_dur_array[i])+' ' + str(space[i])+' '+l_list[i])
         y, sr = sf.read(wav_file)
         data=pyrb.time_stretch(y, sr, wav_dur_array[i]/new_array[i])
         write(concat_string, sr, data)
@@ -129,15 +52,6 @@ for i in range(0,min(len(beats),len(l_list))-1):
     if i > done:
         tfm.build_file(concat_string, concat_string2)
     concat_list.append(concat_string2)
-    #print(concat_string)
-    #print(wav_file)
     
-    
-
-#print(concat_list)
 comb = sox.Combiner()
-comb.build(input_filepath_list=concat_list,output_filepath=sourcepath+'morsh '+str(thresh_ratio_divide)+'.wav',combine_type='concatenate')
-#for i in range(0,len(l_list)):
-    #
-
-'''
+comb.build(input_filepath_list=concat_list,output_filepath=sourcepath+'glad2 test '+str(max_ratio)+'.wav',combine_type='concatenate')
