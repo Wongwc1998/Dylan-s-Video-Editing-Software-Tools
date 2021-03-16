dc1c739cb229704872e56bc4da17d8dc81c72f9f
Composite1618 <37727136+Composite1618@users.noreply.github.com>
2021-03-16 17:49:15 +0800

    Update mid.py
    Used list comprehensions and deleted unwanted variables

--- mid.py
+++ mid.py
@@ -1,56 +1,24 @@
 import numpy as np
 import mido
 
-notes_raw = []
 length_raw = []
 space_raw = []
-notes = []
-length = []
-space = []
-clips = []
-tempo_ratio=[]
-bpm = 135  # song bpm
-path = r'C:/ytpmv/unr/'  # filepath
-midi = 'tunaktunak'
-midi = 'verse2'
-midi = 'verse1'
-midi = 'verse3'
-midi = 'unr'
-
-
-
-concat_list=[]
-concat_string=''
-new_array=[]
-string2=''
-wav_dur_array=[]
-
-thresh_interval=1.0
-thresh_ratio_divide=3.0
-thresh_ratio_merge=thresh_ratio_divide
-long_interval=7.0
-
-
+bpm = 140  # song bpm
+path = r'C:/ytpmv/glados echo/'  # filepath
+midi = 'second'
 
 # mido reads midi file from filepath
 mid = mido.MidiFile(path+midi+'.mid', clip=True)
-for msg in mid.tracks[2]:
-    #print(msg.bytes())
-    if (msg.bytes()[0] == 128):
-        #print(msg.time)
-        length_raw.append(msg.time)
-    if (msg.bytes()[0] == 144):
-        #print(msg.time)
-        space_raw.append(msg.time) 
+length_raw=[msg.time for msg in mid.tracks[2] if (msg.bytes()[0] == 128)]
+space_raw=[msg.time for msg in mid.tracks[2] if (msg.bytes()[0] == 144)]
+#first space is space before note, delete it
 space_raw.pop(0)
+#last note is note of unknown length, change it into some ordinary note
 length_raw[-1]=96
+#add extra note to the end
 length_raw.append(96)
 space_raw.append(0)
-print(length_raw)
-print(space_raw)
 
-for i in range(len(length_raw)-3):
-    length_raw[i]=length_raw[i]+space_raw[i]
-
+length_raw=[a+b for a,b in zip(length_raw, space_raw)]
 
 print(length_raw)
